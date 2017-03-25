a81ac4f66d933351d70dd76c9bdcf17ee5e461e7
pally <t.brussaard@students.uu.nl>
2017-03-25 20:41:56 +0100

    popUp + UI-elements (both features yet to be fully implemented; working)

---
+++ Assets/DisplayInit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayInit : MonoBehaviour {
+    /// <summary>
+    /// Initiatez all used displays
+    /// </summary>
+	void Start ()
+    {
+        Debug.Log(Display.displays.Length);
+        for (int i = 0; i < Display.displays.Length; i++)
+        {
+            Display.displays[i].Activate();
+        }
+        Debug.Log("Displays ACtive: " + Display.displays.Length);
+	}
+}
---
+++ Assets/Scripts/PopUpW.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    
+
+public class PopUpW : MonoBehaviour {
+    private Rect popUpRect;
+    public Canvas canvas;
+    public Vector2 origin;
+    public int width, height;
+    public string message, title;
+    
+	void Start () {
+        origin = gameObject.GetComponent<RectTransform>().anchoredPosition;
+        //GUI.transform.SetParent(canvas.transform);
+        Debug.Log("Hellow World");        
+    }
+
+    void OnGUI()
+    {
+        GUI.Window(0, popUpRect, PopUp, title);
+    }
+
+    void PopUp(int id)
+    {
+        popUpRect = new Rect(origin, new Vector2());
+        Debug.Log(popUpRect);
+        if (GUI.Button(popUpRect, message)) Debug.Log("YATTA");
+    }
+}
