8ad9e49bf2cfaf84352b51e7cecd0c648428a251
pally <t.brussaard@students.uu.nl>
2017-03-30 00:46:31 +0200

    Variables + Economy and UI-Bar [Non-Volatile]

---
+++ Assets/Scripts/Economy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Economy : MonoBehaviour
+{
+    //static variables
+    public static double money = 1000000;
+    public const double startMoney = 1000000;
+    public static double laundredMoney = 0;
+    public static double popularity = 70;
+    public const double startPop = 70;
+    public static double income = 10000;
+    public static double trust = 0.5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        income -= (money / startMoney) * Time.deltaTime;
+        trust -= (popularity / startPop) * Time.deltaTime;
+
+        money += income * Time.deltaTime;
+        popularity += trust * Time.deltaTime;
+
+        //Debug.Log("Money: " + money + "\nPopularity: " + popularity);
+    }
+}
---
+++ Assets/Scripts/MoneyBar.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyBar : MonoBehaviour
+{
+    public GUIStyle MyGUIStyle;
+
+    private void OnGUI()
+    {
+        GUI.Box(new Rect(100, 100, 0.1f * Screen.width * (float)(Economy.money / Economy.startMoney), 0.1f * Screen.height), "MoneyBar", MyGUIStyle);
+    }
+}
